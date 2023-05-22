0835fc542237807500cdcf0c02f709ac9417e5f0
DanielMarella <marella.daniel96@gmail.com>
2023-05-22 10:54:47 +0200

    add 'for' solution

--- snack-3 in classe/js/myscript.js
+++ snack-3 in classe/js/myscript.js
@@ -5,20 +5,31 @@
 // media = somma di tutti i numeri / il numero dei numeri 
 
 
-let  countNumber = 0;
+// let  countNumber = 0;
 
-let sumNumber = 0 ;
+// let sumNumber = 0 ;
 
-let mediaNumber = 0; 
+// let mediaNumber = 0; 
 
-while (countNumber < 10 ){
-    const userNumber = parseInt(prompt('inserisci i primi 10 numeri'));
-    sumNumber += userNumber
-    console.log('sumnumber = ' + sumNumber);
-    countNumber = countNumber + 1;
-    console.log(countNumber);
+// while (countNumber < 10 ){
+//     const userNumber = parseInt(prompt('inserisci i primi 10 numeri'));
+//     sumNumber += userNumber
+//     console.log('sumnumber = ' + sumNumber);
+//     countNumber = countNumber + 1;
+//     console.log(countNumber);
+// }
+
+// mediaNumber = sumNumber / countNumber;
+
+// console.log('mediaNumber = ' + mediaNumber);
+
+// ***************************************************
+// Alternative solution:
+
+let sum = 0;
+
+for (let index = 1; index < 11; index++){
+    sum += index;
 }
 
-mediaNumber = sumNumber / countNumber;
-
-console.log('mediaNumber = ' + mediaNumber);
+console.log(sum, sum / 10);
