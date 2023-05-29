3639bd940dabe443d9c520d221f6a19cfe60cbf1
DanielMarella <marella.daniel96@gmail.com>
2023-05-29 12:11:40 +0200

    try to sum peperoni weight unsucessfull

--- js-snack-peperone/js/myscript.js
+++ js-snack-peperone/js/myscript.js
@@ -1,69 +1,31 @@
 
-// Creare un oggetto che rappresenti un giocatore di basket, con le seguenti proprietà:
-//     Codice giocatore
-//     Nome
-//     Cognome
-//     Età
-//     Media punti fatti per partita
-//     Percentuale di successo per tiri da 3 punti
-//     Stoppate 
-//     Tiri 
 
-// Generare casualmente le statistiche di gioco, secondo queste regole:
-//     il codice giocatore deve essere formato da 3 lettere maiuscole casuali e 3 cifre casuali
-//     la media punti fatti per partita deve essere compresa tra 0 e 50 x
-//     la percentuale di successo per da 3 punti deve essere compresa tra 0 e 100 x
-//     le stoppate sono un numero intero compreso tra 0 e 30 x 
-//     i tiri sono un numero intero compreso tra 20 e 100 x 
+// Crea un array di 10 oggetti che rappresentano un peperone, indicando per ognuno varietà, peso e lunghezza.
+// Calcola quanto pesano tutti i peperoni.
+
+// Bonus: 
+// crea un peperone con una funzione
 
 
 
-// const daniel = {
-    //     codiceGiocatore : 000000,
-    //     nome : 'Daniel',
-    //     cognome : 'Marella',
-    //     età : 27,
-    //     mediaPuntiPartita : 0,
-    //     percentualeSuccesso : 0,
-    //     stoppate: 20,
-    //     tiri: 2
-    // };
-    
-    // basketballPlayer.push(daniel);
-    // console.log(basketballPlayer);
+const typeOfPeperone = [];
 
 
-const basketballPlayer = [];
-    
-let randomMedia = randomPoint(0,50);
-
-let randomPercentual = randomPoint(0, 100);
-
-let randomStop = randomPoint (0, 30);
-
-let randomPull = randomPoint (20, 100)
-
-const daniel = {
-    codiceGiocatore : 000000,
-    nome : 'Daniel',
-    cognome : 'Marella',
-    età : 27,
-    mediaPuntiPartita : randomMedia,
-    percentualeSuccesso : randomPercentual,
-    stoppate: randomStop,
-    tiri: randomPull
-};
-
-basketballPlayer.push(daniel);
-
-console.log(basketballPlayer);
-
-
-function randomPoint(min, max){
-
-    let randomNumber = Math.floor((Math.random() * (max - min) + 1) + min);
-
-    return randomNumber;
+const peperoneVariety = {
+    varietà  : ['giallo', 'rosso', 'verde', 'marrone', 'blu', 'bianco', 'rosa', 'fucsia', 'nero', 'blallo'],
+    peso : ['10g', '11g', '12g', '13g', '14g' ,'15g' ,'16g' ,'17g', '18g', '19g'],
+    lunghezza : ['11cm', '12cm', '13cm', '14cm', '15cm', '16cm', '17cm', '18cm', '19cm', '20cm',]  
 }
+
+let sum = 0;
+for(let i = 0; i < typeOfPeperone.length; i++){
+    sum = peso[i];
+    console.log(sum);
+}
+
+typeOfPeperone.push(peperoneVariety);
+console.log(typeOfPeperone);
+
+
 
 
